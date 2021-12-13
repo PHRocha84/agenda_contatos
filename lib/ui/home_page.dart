ffad8ed3c3f98b658ceb64b76e21a8e8dfa119ee
PHRocha84 <paulinho_spfc8@hotmail.com>
2021-12-13 17:03:00 -0300

    integração cam, e ordenar contatos

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -3,6 +3,10 @@ import 'dart:io';
 import 'package:agenda_contatos/helpers/contact_helper.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+enum OrderOptions {orderaz, orderza}
+
 
 class HomePage extends StatefulWidget {
   @override
@@ -26,6 +30,21 @@ class _HomePageState extends State<HomePage> {
         title: Text("Contatos"),
         backgroundColor: Colors.red,
         centerTitle: true,
+        actions: <Widget>[
+          PopupMenuButton<OrderOptions>(
+            itemBuilder: (context) => <PopupMenuEntry<OrderOptions>>[
+              const PopupMenuItem<OrderOptions>(
+                child: Text("Ordenar de A-Z"),
+                value: OrderOptions.orderaz,
+              ),
+              const PopupMenuItem<OrderOptions>(
+                child: Text("Ordenar de Z-A"),
+                value: OrderOptions.orderza,
+              ),
+            ],
+            onSelected: _orderList,
+          )
+        ],
       ),
       backgroundColor: Colors.white,
       floatingActionButton: FloatingActionButton(
@@ -110,7 +129,11 @@ class _HomePageState extends State<HomePage> {
                           child: Text("Ligar",
                             style: TextStyle(color: Colors.red, fontSize: 20.0),
                           ),
-                          onPressed: () {}
+                          onPressed: () {
+                            launch("tel:${contacts[index].phone}");
+                            Navigator.pop(context);
+
+                          },
                       ),
                     ),
                     Padding(
@@ -169,6 +192,26 @@ class _HomePageState extends State<HomePage> {
         contacts = list;
       });
     });
+  }
+
+  void _orderList(OrderOptions result){
+    switch(result){
+      case OrderOptions.orderaz:
+        contacts.sort((a, b) {
+         return a.name.toLowerCase().compareTo(b.name.toLowerCase());
+        });
+        break;
+      case OrderOptions.orderza:
+        contacts.sort((a, b) {
+       return b.name.toLowerCase().compareTo(b.name.toLowerCase());
+    });
+        break;
+    }
+    setState(() {
+
+    });
+
+
   }
 }
 
